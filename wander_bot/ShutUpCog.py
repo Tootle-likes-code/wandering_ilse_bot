a4171d963e14a551afb19da23ae3579fb1748135
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-09-30 00:24:55 +0100

    Added ShutUpCog.py

---
+++ wander_bot/ShutUpCog.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+import discord
+from discord import User, Member
+from discord.ext import commands
+from discord.ext.commands import Context
+
+
+class ShutUpCog(commands.Cog):
+    def __init__(self, bot: commands.Bot, default_shut_up_id: Optional[int] = None):
+        self.bot = bot
+        self._default_shut_up_id = default_shut_up_id
+
+    @commands.command("shutup")
+    async def shut_up(self, ctx: Context, *, member: str = None):
+        member = await self._validate_member(ctx, member)
+
+        message = "Shut Up"
+        if member is None:
+            message += "!"
+        else:
+            message += f" <@{member.id}>!!"
+
+        await ctx.send(message)
+
+    async def _validate_member(self, ctx: Context, member: str) -> User | Member:
+        if member is not None:
+            return discord.utils.get(ctx.guild.members, username="member")
+        elif self._default_shut_up_id is not None:
+            return await self.bot.fetch_user(self._default_shut_up_id)
+
--- wander_bot/bot.py
+++ wander_bot/bot.py
@@ -1,15 +1,22 @@
+import asyncio
+
 import discord
-from discord import client
 from discord.ext import commands
+
+from wander_bot.ShutUpCog import ShutUpCog
 
 
 class WanderingBot(commands.Bot):
-    def __init__(self, command_prefix='!', intents=None):
+    def __init__(self, command_prefix='!', intents=None, default_shut_up_id=None):
         if intents is None:
             intents = discord.Intents.default()
             intents.message_content = True
 
         super().__init__(command_prefix=command_prefix, intents=intents)
+        asyncio.run(self._add_cogs(default_shut_up_id))
+
+    async def _add_cogs(self, default_shut_up_id):
+        await self.add_cog(ShutUpCog(self, default_shut_up_id))
 
     async def on_ready(self):
         print(f"{self.user} has connected to Discord!!")
