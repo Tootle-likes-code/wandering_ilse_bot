4a609985ee138dfbef082932095f6858b483a9dd
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-09-30 01:33:10 +0100

    Added placeholder BadArgument error handling for roll.

--- wander_bot/dice_roller_cog.py
+++ wander_bot/dice_roller_cog.py
@@ -1,7 +1,13 @@
 from discord.ext import commands
+from discord.ext.commands import BadArgument
 
 
 class DiceRollerCog(commands.Cog):
     @commands.command(name="roll", aliases=["r"])
     async def roll(self, ctx, die_function: str):
         print(die_function)
+
+    @roll.error()
+    async def roll(self, ctx, error):
+        if isinstance(error, BadArgument):
+            await ctx.send(f"You need to give a valid dice string.  Come on.")
