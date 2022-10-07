5b964480d3e47b07e8bcc186578b49b3b6b582d1
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-10-07 19:44:05 +0100

    Integrated pydice into the program.

--- wander_bot/dice_roller_cog.py
+++ wander_bot/dice_roller_cog.py
@@ -1,11 +1,23 @@
+import pydice.dice_string_interpreter
 from discord.ext import commands
-from discord.ext.commands import BadArgument, MissingRequiredArgument
+from discord.ext.commands import MissingRequiredArgument, Context
+from pydice.roll_result import RollResult
 
 
 class DiceRollerCog(commands.Cog):
     @commands.command(name="roll", aliases=["r"])
-    async def roll(self, ctx, die_function: str):
-        print(die_function)
+    async def roll(self, ctx: Context, dice_string: str):
+        result: RollResult = pydice.dice_string_interpreter.interpret(dice_string)
+
+        if result is None:
+            await ctx.send(f"<@{ctx.author.id}>, your dice string doesn't work.  You should check out: " 
+                           f"https://github.com/Tootle-likes-code/pydice/blob/main/README.md")
+        else:
+            message = f"<@{ctx.author.id}> rolled {dice_string}.\n" \
+                      f"`Dice Roll: {result.die_rolls}`\n" \
+                      f"Result: {result.result()}"
+
+            await ctx.send(message)
 
     @roll.error
     async def roll_error(self, ctx, error):
