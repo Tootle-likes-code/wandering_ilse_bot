cfb2f41ab0a596cd4fa17179d2d3c13e1e764886
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-10-22 01:04:18 +0100

    Added cog logic.

--- wander_bot/cogs/merge_messages_cog.py
+++ wander_bot/cogs/merge_messages_cog.py
@@ -1,5 +1,5 @@
 from discord.app_commands import commands
-from discord.ext.commands import Context
+from discord.ext.commands import Context, MissingRequiredArgument
 
 from wander_bot.services.merge_messages_service import MergeMessagesService
 
@@ -8,5 +8,13 @@ class MergeMessagesCog(commands.Cog):
     def __init__(self, service: MergeMessagesService):
         self._service = service
 
-    def watch_channel(self, ctx: Context):
-        pass
+    @commands.command(name="watch")
+    async def watch_channel(self, ctx: Context):
+        self._service.watch_channel(ctx.guild.id, ctx.channel.id)
+
+        await ctx.send(f"Added {ctx.channel.name} to the watch list.")
+
+    @watch_channel.error
+    async def watch_channel_error(self, ctx: Context, error: Exception):
+        if isinstance(error, TypeError):
+            await ctx.send(error.args[0])
