1914cb6fc64140e75c1ec78e6f4b01d5fc427213
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-11-06 02:32:26 +0000

    Added code to validate the user in the cog.

--- wander_bot/cogs/merge_messages_cog.py
+++ wander_bot/cogs/merge_messages_cog.py
@@ -1,16 +1,29 @@
 from discord.ext import commands
 from discord.ext.commands import Context
 
+from wander_bot.services.authentication_service import AuthenticationService
 from wander_bot.services.merge_messages_service import MergeMessagesService
 
 
 class MergeMessagesCog(commands.Cog):
-    def __init__(self, service: MergeMessagesService):
-        self._service = service
+    def __init__(self, merge_service: MergeMessagesService, authentication_services: dict[int, AuthenticationService]):
+        self._merge_service = merge_service
+        self._authentication_services = authentication_services
+
+    def _authenticate_request(self, ctx: Context) -> bool:
+        authentication_service = self._authentication_services.get(ctx.guild.id, None)
+        if not authentication_service:
+            self._authentication_services[ctx.guild.id] = AuthenticationService(ctx.guild)
+            authentication_service = [ctx.guild.id]
+
+        return authentication_service.is_valid_member(ctx.author)
 
     @commands.command(name="watch")
     async def watch_channel(self, ctx: Context):
-        self._service.watch_channel(ctx.guild.id, ctx.channel.id)
+        if not self._authenticate_request(ctx):
+            await ctx.send("You are not able to add a channel to the watchlist.")
+
+        self._merge_service.watch_channel(ctx.guild.id, ctx.channel.id)
 
         await ctx.send(f"Added channel '{ctx.channel.name}' to the watch list.")
 
@@ -21,7 +34,10 @@ class MergeMessagesCog(commands.Cog):
 
     @commands.command(name="stop-watch")
     async def stop_watching_channel(self, ctx: Context):
-        self._service.watch_channel(ctx.guild.id, ctx.channel.id)
+        if not self._authenticate_request(ctx):
+            await ctx.send("You are not able to remove a channel from the watchlist.")
+
+        self._merge_service.watch_channel(ctx.guild.id, ctx.channel.id)
 
         await ctx.send(f"Removed {ctx.channel.name} from watch list.")
 
